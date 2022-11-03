84ea04c79978b529f30ff23197414d901ce1cf18
Shivam-astrotalk <116141476+Shivam-astrotalk@users.noreply.github.com>
2022-11-03 08:00:18 +0530

    little changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+
+
+
     }
 }
